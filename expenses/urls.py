a4b142f277e6f0c30de2301a52f75534339476c9
tushrahuja <ahuja.tushar.m@gmail.com>
2025-01-08 18:57:02 +0530

    Auth and Main app

---
+++ expenses/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+  # path('expenses/',views.index, name="expenses"),
+
+
+
+]
